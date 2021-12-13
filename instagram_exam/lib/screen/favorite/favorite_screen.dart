c3090deb639fa39fc691d702423fbef9dfdae1f3
json-kim <flutter_dart@kakao.com>
2021-12-13 22:31:18 +0900

    chore : 플러터 버전 업그레이드
    업그레이드 and 디렉토리 수정

--- instagram_exam/lib/screen/favorite/favorite_screen.dart
+++ instagram_exam/lib/screen/favorite/favorite_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AlbumScreen extends StatelessWidget {
-  const AlbumScreen({Key key}) : super(key: key);
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('앨범 스크린'),
+        title: Text('좋아요 스크린'),
       ),
       body: Container(),
     );
